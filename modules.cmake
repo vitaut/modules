2293fcb746cbb16b5c82d7aa622b567f77b0217a
Victor Zverovich <victor.zverovich@gmail.com>
2018-06-12 09:14:41 +0200

    Add an option to control the use of modules

--- modules.cmake
+++ modules.cmake
@@ -1,10 +1,26 @@
+# A CMake module that provides functions for using C++ modules in Clang.
+
+option(USE_MODULES "Use C++ modules" OFF)
 set(MODULES_COMPILE_OPTION -fmodules-ts)
+
+# Adds a custom command for building a .pcm file.
+function(add_pcm_build_command mod pcm)
+  add_custom_command(
+    OUTPUT ${pcm}
+    COMMAND ${CMAKE_CXX_COMPILER} ${MODULES_COMPILE_OPTION} -x c++-module
+            --precompile -c -o ${pcm} ${CMAKE_CURRENT_SOURCE_DIR}/${mod}
+    DEPENDS ${mod})
+endfunction()
 
 # Adds an executable compiled with C++ module support.
 # Usage:
 #   add_module_executable(<name> [sources...] MODULES [modules...]
 function(add_module_executable)
   cmake_parse_arguments(AME "" "" "MODULES" ${ARGN})
+  if (NOT USE_MODULES)
+    add_executable(${AME_UNPARSED_ARGUMENTS})
+    return()
+  endif ()
   set(compile_options ${MODULES_COMPILE_OPTION}
       # Clang incorrectly warns abut -fprebuilt-module-path being unused.
       -fprebuilt-module-path=. -Wno-unused-command-line-argument)
@@ -15,12 +31,7 @@ function(add_module_executable)
     set(compile_options ${compile_options} -fmodule-file=${pcm})
     # Use an absolute path to prevent target_link_libraries prepending -l to it.
     set(pcms ${pcms} ${CMAKE_CURRENT_BINARY_DIR}/${pcm})
-    add_custom_command(
-      OUTPUT ${pcm}
-      # Assume that the compiler is clang.
-      COMMAND ${CMAKE_CXX_COMPILER} ${MODULES_COMPILE_OPTION} -x c++-module
-              --precompile -c -o ${pcm} ${CMAKE_CURRENT_SOURCE_DIR}/${mod}
-      DEPENDS ${mod})
+    add_pcm_build_command(${mod} ${pcm})
   endforeach ()
   # Get the target name.
   list(GET AME_UNPARSED_ARGUMENTS 0 name)
@@ -35,6 +46,10 @@ endfunction()
 #   add_module_library(<name> [sources...] MODULES [modules...]
 function(add_module_library)
   cmake_parse_arguments(AME "" "" "MODULES" ${ARGN})
+  if (NOT USE_MODULES)
+    add_library(${AME_UNPARSED_ARGUMENTS})
+    return()
+  endif ()
   set(compile_options ${MODULES_COMPILE_OPTION})
   set(files)
   foreach (mod ${AME_MODULES})
@@ -44,12 +59,7 @@ function(add_module_library)
     set(compile_options ${compile_options} -fmodule-file=${pcm})
     # Use an absolute path to prevent target_link_libraries prepending -l to it.
     set(files ${files} ${CMAKE_CURRENT_BINARY_DIR}/${pcm} ${obj})
-    add_custom_command(
-      OUTPUT ${pcm}
-      # Assume that the compiler is clang.
-      COMMAND ${CMAKE_CXX_COMPILER} ${MODULES_COMPILE_OPTION} -x c++-module
-              --precompile -c -o ${pcm} ${CMAKE_CURRENT_SOURCE_DIR}/${mod}
-      DEPENDS ${mod})
+    add_pcm_build_command(${mod} ${pcm})
     add_custom_command(
       OUTPUT ${obj}
       COMMAND ${CMAKE_CXX_COMPILER} ${MODULES_COMPILE_OPTION} -c -o ${obj} ${pcm}
