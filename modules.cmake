828808c26414766ddee834471c6128118153bf6d
Victor Zverovich <viz@fb.com>
2023-04-11 21:07:58 -0700

    Cleanup config

--- modules.cmake
+++ modules.cmake
@@ -8,6 +8,8 @@ option(USE_MODULES "Use C++ modules" OFF)
 # Usage:
 #   add_pcm_build_commands(<target> <pcms_var> <compile_options_var> <module>...)
 function(add_pcm_build_commands target pcms_var compile_options_var)
+  get_target_property(std ${target} CXX_STANDARD)
+
   set(pcms)
   foreach (mod ${ARGN})
     get_filename_component(pcm ${mod} NAME_WE)
@@ -17,8 +19,9 @@ function(add_pcm_build_commands target pcms_var compile_options_var)
     set(pcms ${pcms} ${CMAKE_CURRENT_BINARY_DIR}/${pcm})
     add_custom_command(
       OUTPUT ${pcm}
-      COMMAND ${CMAKE_CXX_COMPILER} ${MODULES_COMPILE_OPTION} -x c++-module
-              --precompile -c -o ${pcm} ${CMAKE_CURRENT_SOURCE_DIR}/${mod}
+      COMMAND ${CMAKE_CXX_COMPILER}
+              -std=c++${std} -x c++-module --precompile -c
+              -o ${pcm} ${CMAKE_CURRENT_SOURCE_DIR}/${mod}
               "-I$<JOIN:$<TARGET_PROPERTY:${target},INCLUDE_DIRECTORIES>,;-I>"
       # Required by the -I generator expression above.
       COMMAND_EXPAND_LISTS
@@ -37,11 +40,16 @@ function(add_module_executable)
     add_executable(${AME_UNPARSED_ARGUMENTS})
     return()
   endif ()
+
+  # Get the target name.
+  list(GET AME_UNPARSED_ARGUMENTS 0 name)
+  add_executable(${AME_UNPARSED_ARGUMENTS})
+
   # Get the target name.
   list(GET AME_UNPARSED_ARGUMENTS 0 name)
   add_pcm_build_commands(${name} pcms compile_options ${AME_MODULES})
   # Add pcm files as sources to make sure they are built before the executable.
-  add_executable(${AME_UNPARSED_ARGUMENTS} ${pcms})
+  target_sources(${name} PUBLIC ${pcms})
   target_link_libraries(${name} ${pcms})
   target_compile_options(${name} PRIVATE ${compile_options}
       # Clang incorrectly warns about -fprebuilt-module-path being unused.
@@ -57,24 +65,30 @@ function(add_module_library)
     add_library(${AME_UNPARSED_ARGUMENTS})
     return()
   endif ()
+
   # Get the target name.
   list(GET AME_UNPARSED_ARGUMENTS 0 name)
+
+  add_library(${AME_UNPARSED_ARGUMENTS})
+  set_target_properties(${name} PROPERTIES LINKER_LANGUAGE CXX)
+  target_compile_features(${name} PUBLIC cxx_std_20)
+
   add_pcm_build_commands(${name} pcms compile_options ${AME_MODULES})
+
+  # Add pcm files as sources to make sure they are built before the library.
   set(files)
   foreach (pcm ${pcms})
     get_filename_component(pcm_we ${pcm} NAME_WE)
     set(obj ${pcm_we}.o)
     # Use an absolute path to prevent target_link_libraries prepending -l to it.
-    set(files ${files} ${pcm} ${obj})
+    set(files ${files} ${pcm} ${CMAKE_CURRENT_BINARY_DIR}/${obj})
     add_custom_command(
       OUTPUT ${obj}
-      COMMAND ${CMAKE_CXX_COMPILER} ${MODULES_COMPILE_OPTION} -c -o ${obj} ${pcm}
+      COMMAND ${CMAKE_CXX_COMPILER} $<TARGET_PROPERTY:${name},COMPILE_OPTIONS> -c -o ${obj} ${pcm}
       DEPENDS ${pcm})
   endforeach ()
-  # Add pcm files as sources to make sure they are built before the library.
-  add_library(${AME_UNPARSED_ARGUMENTS} ${files})
-  set_target_properties(${name} PROPERTIES LINKER_LANGUAGE CXX)
+  target_sources(${name} PUBLIC ${files})
+
   target_link_libraries(${name} ${pcms})
-  target_compile_features(${name} PUBLIC cxx_std_20)
   target_compile_options(${name} PRIVATE ${compile_options})
 endfunction()
