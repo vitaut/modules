8144c228fa0bb0d05d9ee3dbc02028866d770c4e
Victor Zverovich <victor.zverovich@gmail.com>
2023-04-15 08:10:00 -0700

    Fix formatting

--- modules.cmake
+++ modules.cmake
@@ -33,11 +33,11 @@ function(add_module_library name)
 
   if (CMAKE_CXX_COMPILER_ID MATCHES "Clang")
     # `std` is affected by CMake options and may be higher than C++20.
-    # Clang does not support c++23/c++26 names, so replace it with 2b
+    # Clang does not support c++23/c++26 names, so replace it with 2b.
     get_target_property(std ${name} CXX_STANDARD)
-    if(std GREATER 20)
-        set(std 2b)
-    endif()
+    if (std GREATER 20)
+      set(std 2b)
+    endif ()
 
     set(pcms)
     foreach (src ${sources})
