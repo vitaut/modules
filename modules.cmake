226006638198f6531e57ed22a965b62805e8af16
worthless443 <aissypewdiepie@gmail.com>
2023-05-03 09:42:15 +0530

    * added support for using standard headers as modules * if used the import syntax with standard headers, and the compiler is   Clang, cmake will fail (don't yet know how to do it with Clang) * added a CMake command that shall be used either when using std   headers in the main executable or want to use std headers as   executables and compile it soley (without other moduler libraries)

--- modules.cmake
+++ modules.cmake
@@ -30,6 +30,11 @@
   without including the above copyright and permission notices.
 ]]
 
+# needed flags for GCC
+if (CMAKE_CXX_COMPILER_ID MATCHES "GNU")
+	set(GENERIC_CXX_MODULE_FLAGS -std=c++20 -fcoroutines -fmodules-ts -xc++-system-header)
+	set(GCM_CACHE gcm.cache/usr/include/c++/12.2.1)
+endif()
 
 # Clang requires the CXX_EXTENSIONS property to be set to false to use modules.
 # If the user has not set it explicitly, do it here. Otherwise warn if it is not
@@ -51,6 +56,79 @@ endif ()
 #   if (<variable_name> GREATER 17)
 #     ...
 #   endif ()
+
+# wth regex checks if we used import syntax importing standard headers as modules and sets a var if gets any with a list of standard headers 
+function(check_no_import_stdlib result) 
+	set(stdlib_list iostream string vector) # add the entire list of standard headers?
+	cmake_parse_arguments(MOD "" "IF" "Else" ${ARGN})
+	set(files ${MOD_UNPARSED_ARGUMENTS})
+	set(modules)
+	foreach(file ${files})
+		file(READ ${file} contents)
+		string(REGEX REPLACE "\n" ";" lines ${contents}) 
+		foreach(line ${lines})
+			string(REGEX MATCH "import.*" import ${line})
+			if(NOT import STREQUAL "")
+				string(REGEX REPLACE " " ";" conts ${import})
+				list(GET conts 1 module)
+				set(modules ${modules} ${module})
+			endif()
+		endforeach()
+			
+	endforeach()
+
+	set(${result} FALSE PARENT_SCOPE)
+	foreach(module ${modules})
+		string(REGEX MATCH "\<.*\>" match ${module})
+		if(NOT match STREQUAL "")
+			set(${result} TRUE PARENT_SCOPE)
+		endif()
+		if(${module} IN_LIST stdlib_list)
+			set(${result} TRUE PARENT_SCOPE)
+		endif()
+	endforeach()
+	set(std_modules ${modules} PARENT_SCOPE)
+endfunction()
+
+# generate the std gcm files required for using standard as modules
+function(add_stdheader_gcm)
+	cmake_parse_arguments(MOD "" "IF" "Else" ${ARGN})
+	set(source ${MOD_UNPARSED_ARGUMENTS})
+	check_no_import_stdlib(has_stdimport ${source})
+	set(STD_HEADERS_BUILT FALSE PARENT_SCOPE)
+	set(CMAKE_CXX_EXTENSIONS OFF PARENT_SCOPE)
+	list(LENGTH std_modules len_std_modules)	
+	if(len_std_modules GREATER 0)
+		foreach(std_module ${std_modules})
+			string(REGEX MATCH "\<.*\>" match ${std_module})
+			if(NOT match STREQUAL "")
+				string(REGEX MATCH "[a-z]+" module ${std_module})
+				add_custom_target(${module} ALL)
+				add_custom_command(
+					TARGET ${module}
+					COMMAND ${CMAKE_CXX_COMPILER} ${GENERIC_CXX_MODULE_FLAGS} ${module}
+				)
+				set(STD_HEADERS_BUILT TRUE)
+			endif()
+		endforeach()
+	endif()
+	if(CMAKE_CXX_COMPILER_ID MATCHES "Clang" AND STD_HEADERS_BUILT)
+		message(FATAL_ERROR "std modules with Clang is not supported yet")
+	endif()
+endfunction()
+
+function(add_stdmodular_executable name)
+	cmake_parse_arguments(MOD "" "IF" "Else" ${ARGN})
+	set(source ${MOD_UNPARSED_ARGUMENTS})
+	if(NOT STD_HEADERS_BUILT)
+		add_stdheader_gcm(${source})	
+	endif()
+	add_executable(${name} ${source})
+	target_compile_features(main PUBLIC cxx_std_20)
+	target_compile_options(main PUBLIC -std=c++20 -fcoroutines -fmodules-ts)
+endfunction()
+
+
 function(modules_get_latest_cxx_std result)
   # Assume that 98 will be supported even with a broken feature detection.
   set(std_version 98)
@@ -131,6 +209,14 @@ endfunction()
 function(add_module_library name)
   cmake_parse_arguments(AML "" "IF" "FALLBACK" ${ARGN})
   set(sources ${AML_UNPARSED_ARGUMENTS})
+
+  if(has_stdimport AND ${CMAKE_CXX_COMPILER_ID} MATCHES "Clang")
+	  message(FATAL_ERROR "Sorry! Clang is not supported yet")
+  endif()
+
+  if(NOT STD_HEADERS_BUILT)
+	add_stdheader_gcm(${sources})	
+  endif()
 
   add_library(${name})
   set_target_properties(${name} PROPERTIES LINKER_LANGUAGE CXX)
