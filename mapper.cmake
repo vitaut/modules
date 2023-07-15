37a1c2f4f707edc4f59edca5ee60e36a69d8ebef
islam omar ahmed <io1131@fayoum.edu.eg>
2023-07-15 21:11:07 +0300

    update

--- mapper.cmake
+++ mapper.cmake
@@ -1,4 +1,4 @@
-write_file(${MAPPER_FILE} "")
+write_file(${MAPPER_FILE} "") # TODO: merge all libs files
 FILE(GLOB children RELATIVE ${CMAKE_CURRENT_BINARY_DIR}/gcm.cache/ ${CMAKE_CURRENT_BINARY_DIR}/gcm.cache/*)
 FOREACH(child ${children})
   IF(NOT (IS_DIRECTORY ${CMAKE_CURRENT_BINARY_DIR}/gcm.cache/${child}))
